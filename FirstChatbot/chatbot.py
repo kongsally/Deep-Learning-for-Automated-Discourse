918d096038c6285fd35d1cc4c18b8383720636ca
yoninachmany <yoninachmany@gmail.com>
2017-02-16 12:27:52 -0500

    chatbot uses new fxn

--- FirstChatbot/chatbot.py
+++ FirstChatbot/chatbot.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from flask import Flask, render_template
 from flask_ask import Ask, request, statement, question, session
-from converse import decode
+from converse import decode, decode_init
 
 
 app = Flask(__name__)
@@ -11,6 +11,8 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 FLAGS = tf.app.flags.FLAGS
+
+sess = tf.Session()
 
 @ask.launch
 def launch():
@@ -20,8 +22,9 @@ def launch():
 @ask.intent("AnswerIntent", mapping={'utterance':'Text'})
 def answer(utterance):
     stripped_utterance = utterance.rstrip()
-    chat_bot_response = decode(stripped_utterance)
+    chat_bot_response = decode(stripped_utterance, model, en_vocab, rev_fr_vocab, sess)
     return statement(chat_bot_response)  # what if a question?
 
 if __name__ == '__main__':
+    (model, fr_vocab_path, en_vocab, rev_fr_vocab, sess) = decode_init(sess)
     app.run(debug=True)
