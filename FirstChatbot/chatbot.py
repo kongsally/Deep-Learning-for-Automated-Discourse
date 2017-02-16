fafcedb1ebaf41b22548c0286ca78bf827982bd0
yoninachmany <yoninachmany@gmail.com>
2017-02-15 19:38:57 -0500

    changed name in order to copy seq2seq code

--- FirstChatbot/chatbot.py
+++ FirstChatbot/chatbot.py
@@ -1,6 +1,6 @@
 import logging
 import tensorflow as tf
-import translate
+import translateSteps
 
 from flask import Flask, render_template
 from flask_ask import Ask, request, statement, question, session
@@ -16,7 +16,7 @@ FLAGS = tf.app.flags.FLAGS
 
 with tf.Session() as sess:
     # Create model and load parameters.
-    (model, in_vocab, out_vocab) = translate.init_decode(sess)
+    (model, in_vocab, out_vocab) = translateSteps.init_decode(sess)
 
 @ask.launch
 def launch():
@@ -26,7 +26,7 @@ def launch():
 @ask.intent("AnswerIntent")
 def answer(utterance):
     stripped_utterance = utterance.rstrip()
-    chat_bot_response = str(translate.decode_sentence(sess, model, in_vocab, out_vocab, stripped_utterance))
+    chat_bot_response = str(translateSteps.decode_sentence(sess, model, in_vocab, out_vocab, stripped_utterance))
     return statement(chat_bot_response)  # what if a question?
 
 if __name__ == '__main__':
---
+++ FirstChatbot/translateSteps.py
@@ -0,0 +1,9 @@
+"""
+single layer LSTM
+1024 memory cells
+stochastic gradient descent
+
+vocabulary: most common 20K words (special tokens indicating turn taking, actor)
+
+convergence: perplexity of 8
+"""
