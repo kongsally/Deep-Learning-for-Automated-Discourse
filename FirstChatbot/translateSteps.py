bb6791d0d189e69df0adebbf056b78749c175291
yoninachmany <yoninachmany@gmail.com>
2017-02-15 22:11:01 -0500

    delete steps

--- FirstChatbot/translateSteps.py
+++
@@ -1,9 +0,0 @@
-"""
-single layer LSTM
-1024 memory cells
-stochastic gradient descent
-
-vocabulary: most common 20K words (special tokens indicating turn taking, actor)
-
-convergence: perplexity of 8
-"""
