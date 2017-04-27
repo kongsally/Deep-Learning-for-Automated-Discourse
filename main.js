18093b97962a9399cee324eeb2884b0a4a195c05
Sally Kong <kongsally@users.noreply.github.com>
2017-04-27 15:28:39 -0400

    Updated website with sentiment analysis results

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 
-var cornellPath = "./cornellResponses/";
-var twitterPath = "./twitterResponses/";
-var cornellTwiiterPath = "./cornellTwitterResponses/";
+var pos2Path = "./2layer-pos/"
+var pos3Path = "./3layer-pos/"
+var neg2Path = "./2layer-neg/"
+var neg3Path = "./3layer-neg/"
 
 function loadText(path, iterationNumber, targetDiv) {
-	var fileName = "corrected_dialogue_a_" + iterationNumber;
+	var fileName = "dialogue_a_" + iterationNumber;
 	$.get(path + fileName, function(data) {
 	    var lines = data.split("\n");
 	    $.each(lines, function(n, elem) {
-	    	if (n > 1) {
-	    		if (n%2 == 0) {
+	    	if (n > 0) {
+	    		if (n%2 == 1) {
 	    			$('#' + targetDiv).append('<p class="question">' + elem + '</p>');
 	        	} else {
 	        		$('#' + targetDiv).append('<p class="answer"> -> ' + elem + '</p>');
 	        	}
-	    	} else if (n == 1) {
-	    		$('#' + targetDiv + "Perplexity").empty();
-	    		$('#' + targetDiv + "Perplexity").append('<p class="properties">' + elem + '</p>');
-	    	}
+	    	} 
 	    });
     });
 }
 
 function init() {
-	$( function() {
+	$(function() {
     	$( "#selectable" ).selectable();
- 	 } ); 
+ 	}); 
 }
 
 function loadDialogues(checkptNumber) {
-	
-	$("#cornell").empty();
-	$("#twitter").empty();
-	$("#cornellTwitter").empty();
+	$("#pos2").empty();
+	$("#pos3").empty();
+	$("#neg2").empty();
+	$("#neg3").empty();
 
-	loadText(cornellPath, checkptNumber, "cornell");
-	loadText(twitterPath, checkptNumber, "twitter");
-	loadText(cornellTwiiterPath, checkptNumber, "cornellTwitter");
+	loadText(pos2Path, checkptNumber, "pos2");
+	loadText(pos3Path, checkptNumber, "pos3");
+	loadText(neg2Path, checkptNumber, "neg2");
+	loadText(neg3Path, checkptNumber, "neg3");
 }
